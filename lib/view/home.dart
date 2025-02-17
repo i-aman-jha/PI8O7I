8fccbb22542c27775c544de27951be3e253720d7
i-aman-jha <amanjha.7096@gmail.com>
2025-02-18 03:39:54 +0530

    fixed layout issues

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -11,6 +11,8 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(WidgetController());
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final bool isSmallScreen = deviceHeight < 520;
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -27,9 +29,11 @@ class Home extends StatelessWidget {
                     height: (kIsWeb)
                         ? MediaQuery.of(context).size.height * 0.7
                         : MediaQuery.of(context).size.height * 0.6,
-                    width: (kIsWeb)
-                        ? MediaQuery.of(context).size.height * 0.6
-                        : MediaQuery.of(context).size.width * 0.9,
+                    width: (isSmallScreen)
+                        ? MediaQuery.of(context).size.width * 0.7
+                        : (kIsWeb)
+                            ? MediaQuery.of(context).size.height * 0.6
+                            : MediaQuery.of(context).size.width * 0.9,
                     padding: const EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Color.fromARGB(71, 0, 255, 94),
@@ -45,10 +49,31 @@ class Home extends StatelessWidget {
                               ),
                             ),
                           )
-                        : Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: controller.widgetList.value,
-                          )),
+                        : (isSmallScreen)
+                            ? LayoutBuilder(builder: (context, constraints) {
+                                return SingleChildScrollView(
+                                  child: Column(
+                                    children: controller.widgetList.value
+                                        .map((widget) {
+                                      if (widget is Spacer) {
+                                        return SizedBox(
+                                            height:
+                                                20); // or any desired height
+                                      }
+                                      return Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 8.0),
+                                        child: widget,
+                                      );
+                                    }).toList(),
+                                  ),
+                                );
+                              })
+                            : Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: controller.widgetList.value,
+                              )),
               ),
               SizedBox(
                 height: (kIsWeb) ? 40 : 20,
